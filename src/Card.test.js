f812e358875d442f5fe96d3538f2ac7e6aa98cf2
Chong <Chongkb03@gmail.com>
2018-07-10 15:37:08 +0800

    Initial commit

---
+++ src/Card.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+import {shallow, mount, render} from 'enzyme';
+
+import Card from './Card';
+
+
+it('expect to render card component', () => {
+
+	expect(shallow(<Card />).length).toEqual(1)
+})
